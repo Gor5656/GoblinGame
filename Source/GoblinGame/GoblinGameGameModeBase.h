e387f7974bc2a48a57f6953f91ed24cade034470
Gor5656 <gorkocharyan.1113@gmail.com>
2023-11-09 14:04:42 +0400

    Initial commit

---
+++ Source/GoblinGame/GoblinGameGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "GoblinGameGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GOBLINGAME_API AGoblinGameGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
